701da3c0d1dff7b5d1124c45daca3821591949a3
moaz korena <moazkorena@gmail.com>
2018-09-24 18:55:57 +0800

    Externalized configuration and building of third party libraries.
    Because I will never submit to the unreasonable workflow of building
    my own service code as and external project, because it is not an external project.
    
    - cms works
    - grpc works

---
+++ modules/FindActiveMqCpp.cmake
@@ -0,0 +1,27 @@
+# Try to find activemq-cpp library
+# Once done this will define
+#  PART4_FOUND - if system found activemq-cpp library
+#  ACTIVEMQCPP_INCLUDE_DIRS - The activemq-cpp include directories
+#  ACTIVEMQCPP_LIBRARIES - The libraries needed to use Part4
+#  ACTIVEMQCPP_DEFINITIONS - Compiler switches required for using activemq-cpp
+
+find_path(ACTIVEMQCPP_INCLUDE_DIR
+        NAMES decaf/lang/Thread.h
+        PATHS /usr/local/include/activemq-cpp-3.9.4)
+
+find_library(ACTIVEMQCPP_LIBRARY NAMES libactivemq-cpp.a libactivemq-cpp.la libactivemq-cpp.dylib libactivemq-cpp.19.dylib SHARED IMPORTED GLOBAL)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(ACTIVEMQCPP DEFAULT_MSG ACTIVEMQCPP_INCLUDE_DIR ACTIVEMQCPP_LIBRARY)
+
+if (ACTIVEMQCPP_FOUND)
+    set(ACTIVEMQCPP_LIBRARIES ${ACTIVEMQCPP_LIBRARY} )
+    set(ACTIVEMQCPP_INCLUDE_DIRS ${ACTIVEMQCPP_INCLUDE_DIR} )
+    set(ACTIVEMQCPP_DEFINITIONS )
+else ()
+  MESSAGE(STATUS "ACTIVEMQCPP NOT FOUND")
+endif ()
+
+mark_as_advanced(ACTIVEMQCPP_ROOT_DIR ACTIVEMQCPP_INCLUDE_DIR ACTIVEMQCPP_LIBRARY)
---
+++ modules/FindApr.cmake
@@ -0,0 +1,26 @@
+# Apache Portable Runtime
+
+set (APR_ROOT_DIR /usr/local/opt/apr/libexec)
+
+find_path(APR_INCLUDE_DIR
+        NAMES apr_pools.h
+        PATHS ${APR_ROOT_DIR}/include/apr-1)
+
+find_library(APR_LIBRARY 
+        NAMES libapr-1.a apr.exp libapr-1.0.dylib libapr-1.a libapr-1.dylib
+        PATHS ${APR_ROOT_DIR}/lib SHARED IMPORTED GLOBAL)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(APR DEFAULT_MSG APR_INCLUDE_DIR APR_LIBRARY)
+
+if (APR_FOUND)
+    set(APR_LIBRARIES ${APR_LIBRARY} )
+    set(APR_INCLUDE_DIRS ${APR_INCLUDE_DIR} )
+    set(APR_DEFINITIONS )
+else ()
+  MESSAGE(STATUS "APR NOT FOUND")
+endif ()
+
+mark_as_advanced(APR_ROOT_DIR APR_INCLUDE_DIR APR_LIBRARY)
